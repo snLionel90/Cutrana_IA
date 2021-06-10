7397052483deb01984832d6aa09ee07ab728a3f4
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2021-06-10 20:06:54 +0200

    updated code with more funcions

--- Cutrana.py
+++ Cutrana.py
@@ -1,25 +1,66 @@
-#pip install pipwin
-#pip install speechrecognition 
-#pip install --upgrade speechrecognition
+import os
 import webbrowser
 import speech_recognition as sr
-r = sr.Recognizer() 
+import pywhatkit
+import urllib.request
+import json
+import datetime
+import pyttsx3
+
+__Author__ = "snlionel90"
+engine = pyttsx3.init()
+reg = sr.Recognizer() 
+
+# get voices and set the first of them
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+# editing default configuration
+engine. setProperty('rate', 178)
+engine.setProperty('volume', 0.7)
+
 while True:
     with sr.Microphone() as source:
-        print('Hola, soy tu asistente por voz, Cutrana, en que pueedo ayudarle?: ')
-        audio = r.listen(source)
+        talk('Hola,'+__Author__+ 'soy Cutrana, en que pueedo ayudarle?: ')
+        audio = reg.listen(source)
  
         try:
-            text = r.recognize_google(audio)
-            print('Has dicho: {}'.format(text))
-            print(text)
-            if "Google" in text:
+            order = reg.recognize_google(audio, language='es-ES')
+            talk('Has dicho: {}'.format(order))
+            talk(order)
+
+            #bloque de condiciones al hablar por el micro
+            if "Google" in order:
+                talk("Abriendo Google")
                 webbrowser.open('https://google.es')
-            if "Amazon" in text:
+                
+            elif "Amazon" in order:
                 webbrowser.open('https://amazon.es')
-            if "noticias" in text:
+
+            elif "noticias" in order:
                 webbrowser.open('https://20minutos.es')
-            if "que tal" in text:
-                print("Bien y tu?")
+
+            elif "buscar en la wiki" in order:
+                wikipedia.set_lang("es")
+                info = wikipedia.summary(order, 1)
+                talk(info)
+
+            elif "que tal" in order:
+                talk("Bien y tu?")
+
+            elif "Cuentame un chiste" in order:
+                talk("lUL")
+
+            elif "ponme musica" in order:
+                #path = "C:/Program Files (x86)/Windows Media Player/"
+                #os.chdir(path)
+                os.system("wmplayer.exe")
+                talk("aaaaaa")
+
+            elif "hasta Luego" in order:
+                talk("hasta luego" +__Author__)
+
+            else:
+                talk("No se ha entendio nada")
         except:
-            print('No entendel, ¿me lo puedes repetir por favor?')
+            talk('No entendel, ¿me lo puedes repetir por favor?')
