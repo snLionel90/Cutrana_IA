6662205025ec3c4b81241bfd7a38814a1acd8f87
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2021-06-13 17:22:08 +0200

    new code

--- Cutrana.py
+++ Cutrana.py
@@ -1,57 +1,70 @@
-import os
-import webbrowser
 import speech_recognition as sr
-import pywhatkit
-import urllib.request
-import json
-import datetime
 import pyttsx3
-
+import pywhatkit
+import requests, json, sys,os, datetime
+import webbrowser
+import time
+import subprocess
 
 __Author__ = "snlionel90"
 
-reg = sr.Recognizer() 
+print('Opening....')
 
-while True:
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[0].id')
+
+def spk(txt):
+    engine.say(txt)
+    engine.runAndWait()
+
+
+def comands():
+    reg = sr.Recognizer()
     with sr.Microphone() as source:
-        print('Hola,'+__Author__+ 'soy Cutrana, en que pueedo ayudarle?: ')
-        audio = reg.listen(source)
- 
+        print("Listening...")
+        sound=reg.listen(source)
+
         try:
-            order = reg.recognize_google(audio)
-            print('Has dicho: {}'.format(order))
-            print(order)
+            status=reg.recognize_google(sound,key = None,language='en-US',show_all=False)
+            print(f"user said:{status}\n")
 
-            #bloque de condiciones al hablar por el micro
-            if "Google" in order:
-                print("Abriendo Google")
-                webbrowser.open('https://google.es')
-                
-            if "Amazon" in order:
-                webbrowser.open('https://amazon.es')
+        except Exception as e:
+            spk("Lo siento, pero no e entendido la petición, repitemela por favor")
+            print("Lo siento, pero no e entendido la petición, repitemela por favor")
+            return "None"
+        return status
 
-            if "noticias" in order:
-                webbrowser.open('https://20minutos.es')
+if __name__=='__main__':
 
-            if "buscar en la wiki" in order:
-                wikipedia.set_lang("es")
-                info = wikipedia.summary(order, 1)
-                print(info)
+    while True:
+        spk("Hola, ese ene lionel noventa. soy Cutrana, el asistente por voz, en que puedo ayudarle?")
+        print("Hola snLionel90,soy Cutrana, el asistente por voz, en que puedo ayudarle?")
+        status = comands().lower()
+        if status ==0:
+            continue
 
-            if "que tal" in order:
-                print("Bien y tu?")
+        if "open news " in status or "ultima hora" in status:
+            webbrowser.open('https://20minutos.es')
+            spk('Abruiendo el panel de noticias')
+            time.sleep(5)
 
-            if "A" in order:
-                print("lUL")
+        elif "open google" in status:
+            webbrowser.open_new_tab ('https://google.es')
+            spk('Abriendo el navegador de internet, con google')
+            time.sleep(6)
+        
+        elif "open job" in status:
+            webbrowser.open_new_tab ('https://www.linkedin.com/?trk=public_jobs_nav-header-logo')
+            spk('abriendo linkedin')
+            time.sleep(4)
 
-            if "ponme musica" in order:
-                #path = "C:/Program Files (x86)/Windows Media Player/"
-                #os.chdir(path)
-                os.system("wmplayer.exe")
-                print("aaaaaa")
+        elif "log off" in status or "apagar" in status:
+            spk("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+            subprocess.call(["shutdown", "/l"])
+            time.sleep(5)
+    
+time.sleep(3)
 
-            if "hasta Luego" in order:
-                print("hasta luego" +__Author__)
 
-        except:
-            print('No entendel, ¿me lo puedes repetir por favor?')
+
